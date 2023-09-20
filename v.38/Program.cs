25f0dd2db38bf68ef03fcc6d04abb1ede1cc76b8
Bledon <bledonqyqalla@gmail.com>
2023-09-20 15:20:02 +0200

    .ignore fil add

---
+++ v.38/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Diagnostics.Metrics;
+
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("v38");
+Console.WriteLine(23 + 23);
+string text = "textual";
+
+
+Console.WriteLine(text);
+
+
+
+
+
+
+// Fråga 1.
+string name = v._38.Services.Utility.GetUserName();
+
+// Fråga 2.
+string word = v._38.Services.Utility.GetWord();
+
+
+//Fråga 3.
+string age = v._38.Services.Utility.GetNumber("Enter Your Age");
+
+
+// Fråga 4.
+
+string hobby = v._38.Services.Utility.AskHobby("Hobby");
+
+// Fråga 5.
+string fullName = v._38.Services.Utility.Getinfo("firstName", "lastName");
+
+Console.WriteLine(name);//Svar 1. 
+
+Console.WriteLine(word);// Svar 2. 
+
+Console.WriteLine(age); //Svar 3.
+
+Console.WriteLine($"{hobby} sounds boring"); // Svar 4.
+
+Console.WriteLine(fullName);  // svar 5.
+Console.ReadLine();
---
+++ v.38/Services/Utility.cs
@@ -0,0 +1,49 @@
+﻿using System;
+namespace v._38.Services
+{
+	public class Utility
+	{
+		//Method 1. 
+		public static string GetUserName()
+		{
+			Console.WriteLine("Enter Username");
+			string userName = Console.ReadLine();
+			return userName;
+		}
+		//Method 2.
+		public static string GetWord()
+		{
+			Console.WriteLine("Enter Word");
+			string word = Console.ReadLine();
+			return "#"+ word;
+		}
+		//Method 3.
+		public static string GetNumber(string message)
+		{
+			Console.WriteLine(message);
+			string age = Console.ReadLine();
+			return age;
+		}
+		//Method 4.
+		public static string AskHobby( string word)
+		{
+			Console.WriteLine( $"Please Enter A {word}");
+			string hobby = Console.ReadLine();
+			return hobby;
+		}
+        //Method 5.
+        public static string Getinfo( string firstName, string lastName)
+        {
+	
+			return $" {lastName}, {firstName}";
+
+        }
+		
+
+
+
+
+
+    }
+}
+
